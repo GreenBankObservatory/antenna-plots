48f8ee13d2f610e455e57f0a4a23e741639d3463
Elizabeth Lowe <elowe@nrao.edu>
2022-05-27 13:39:07 -0400

    Messing with marker size

--- first_plot.py
+++ first_plot.py
@@ -18,10 +18,13 @@ dec = Angle(antenna_data['DECJ2000']*u.degree)
 
 fig = plt.figure(figsize=(12,10))
 ax = fig.add_subplot(111, projection="mollweide")
-ax.scatter(ra.radian, dec.radian, marker=".", s=.4, )
+ax.scatter(ra.radian, dec.radian,marker="o", facecolor='Green',edgecolor='Black',linewidth=2, s=7, alpha=.08)
 ax.set_xticklabels(['14h','16h','18h','20h','22h','0h','2h','4h','6h','8h','10h'])
+ax.set_xlabel("RAJ2000")
+ax.set_ylabel("DECJ2000")
 ax.grid(True)
+plt.title("GBT")
 
-
-#fig.savefig("map3.png")
+#fig.savefig("map4.png")
 plt.show()
+
